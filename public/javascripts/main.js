e06d15987476a745f0796ba89cd11257f9ef3768
A-Tetarwal <andashishwill@gmail.com>
2024-09-15 15:35:35 +0530

    All files

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,19 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const progressCircle = document.getElementById("progress-circle");
   let seconds = 59;
   const maxSeconds = 59;
-  const circumference = 2 * Math.PI * 18; // Updated radius to 18 for smaller circle
+  const circumference = 2 * Math.PI * 18;
   progressCircle.style.strokeDasharray = `${circumference}`;
 
   function updateTimer() {
     timerElement.textContent = seconds < 10 ? `0${seconds}` : seconds;
 
-    // Calculate progress
     const offset = circumference - (seconds / maxSeconds) * circumference;
     progressCircle.style.strokeDashoffset = offset;
 
-    // Decrease the seconds
     if (seconds === 0) {
-      seconds = maxSeconds; // Reset to 59 seconds if 0
+      seconds = maxSeconds;
     } else {
       seconds--;
     }
